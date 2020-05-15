e08fca946d9ba5023b483782827a34b06feada23
TechAviator004 <khany4567@gmail.com>
2020-05-15 17:12:58 +0530

    my python file

---
+++ python.py
@@ -0,0 +1,18 @@
+import turtle
+
+mt = turtle.Turtle()
+mt.forward(100)
+mt.right(90)
+mt.forward(100)
+mt.right(78)
+mt.forward(100)
+mt.left(78)
+mt.forward(100)
+mt.left(90)
+mt.forward(100)
+mt.left(90)
+mt.forward(100)
+mt.left(78)
+mt.forward(100)
+mt.right(78)
+mt.forward(100)
